f72453371a0b8f59cc0a044739f884da8e96b2c1
PC trabajo <miguelandresbuitragocruz@gmail.com>
2022-08-03 18:00:38 -0500

    Clase escribiendo HTML

--- main.js
+++ main.js
@@ -1,6 +1,47 @@
-console.log("Hola mundo");
-console.log("Hola mundo x2 edicion desde GitHub");
+// Si se quiere seleccionar elemento por tipo, se utiliza querySelector("tipo")
 
-console.log("Problema de GitHub Solucionado");
+const h1 = document.querySelector("h1");
 
-console.log("Prueba de conexion desde VS Code");
+// Si se quiere seleccionar elemento por tipo, se utiliza querySelector("tipo")
+
+const p = document.querySelector("p");
+
+// Si se quiere seleccionar elemento por clase, se utiliza querySelector(".clase")
+
+const parrafito = document.querySelector(".parrafito");
+
+// Si se quiere seleccionar elemento por id, se utiliza querySelector("#id")
+
+const pid = document.querySelector("#pid");
+
+// Si se quiere seleccionar elemento por tipo, se utiliza querySelector("tipo")
+
+const input = document.querySelector("input");
+
+console.log(h1);
+
+console.log({
+    h1,
+    p,
+    parrafito,
+    pid,
+    input
+});
+
+// Propiedad .innerHTML permite modificar HTML desde javascript con riesgos de encriptacion
+h1.innerHTML = "Nuevo titulo editado desde javascript <br> Mejorado"
+
+// Propiedad .innerText permite modificar texto de HTML desde javascript sin riesgos de encriptacion
+h1.innerText = "Nuevo titulo editado desde javascript <br> Mejorado"
+
+// .getAttribute lee los atributos del elemento
+h1.getAttribute("pantalla");
+h1.getAttribute("class");
+
+console.log(h1.getAttribute("pantalla"));
+console.log(h1.getAttribute("class"));
+
+// .setAttribute modifica los atributos del elemento
+h1.setAttribute("class", "rojo");
+
+// Continuar clase "Escribiendo HTML desde JavaScript" minuto 7:00
